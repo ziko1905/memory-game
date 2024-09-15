761d51264f9b6623929a86bcbb9336b05ba58948
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-15 03:24:56 +0200

    Create logic for fetching imgs

---
+++ src/fetchImages.js
@@ -0,0 +1,20 @@
+const SPORTS = ['tennis', 'football', 'basketball', 'skiing', 'mtb', 'surfing', 'boxing', 'swimming', 'running', 'hockey', '8 ball', 'horse racing']
+const API_KEY = 'hQDKrYtq5inxyRKMHrVnMNxxpHaZ01Y6'
+
+
+async function getImgSrc(s) {
+    return await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${s}`, {mode: "cors"})
+        .then(resolve => resolve.json())
+        .then(resolve => resolve)
+}
+
+async function getCards() {
+    const cards = [SPORTS.map((value, index) => {
+        return {id: index, url: getImgSrc(value).then(response => response)}}
+    )]
+    cards.push({url: new Promise((resolve) => setTimeout(resolve, 5000))})
+    await Promise.all(cards.map(card => card.url))
+    return cards
+}
+
+console.log(await getCards())
