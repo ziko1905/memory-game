621c0d857d7b50dd16e7ca8e8260561ce8628f75
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-15 06:10:48 +0200

    Add basic styling

--- src/components/GameScore.jsx
+++ src/components/GameScore.jsx
@@ -1,9 +1,11 @@
+import "../styles/GameScore.css"
+
 export function GameScore() {
     return (
         <div className="game-score-div">
             <h1>Sport Meme Memory Game</h1>
             <h3>Best Score: </h3>
-            <h3>Curr Score: </h3>
+            <h3>Current Score: </h3>
         </div>
     )
 }
