82d76ad4f2565f8a28ac815a83c7e29e53ee1211
Karlo Cehulic <karlocehlic@gmail.com>
2024-09-15 05:44:05 +0200

    Create game score comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import './fetchImages.js'
-import { CardGrid } from './components/CardGrid.jsx'
+import { CardGrid } from './components/CardGrid'
+import { GameScore } from './components/GameScore'
 
 export function App() {
   console.log("App opened")
   return (
     <div className="game-div">
+      <GameScore />
       <CardGrid />
     </div>
   )
---
+++ src/components/GameScore.jsx
@@ -0,0 +1,9 @@
+export function GameScore() {
+    return (
+        <div className="game-score-div">
+            <h1>Sport Meme Memory Game</h1>
+            <h3>Best Score: </h3>
+            <h3>Curr Score: </h3>
+        </div>
+    )
+}
